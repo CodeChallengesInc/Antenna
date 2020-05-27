9046fffa40aca885e03b0d71df5558afebdcbda9
Jonathan Ferrin <Jonathan.Ferrin@hyland.com>
2020-05-27 13:52:51 -0400

    new better initial stuff

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ants'`, () => {
+  it(`should have as title 'Frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ants');
+    expect(app.title).toEqual('Frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ants app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Frontend app is running!');
   });
 });
