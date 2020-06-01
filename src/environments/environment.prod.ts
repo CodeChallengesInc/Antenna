3d73f393971fe34e64449e11abce6cb060fe13b3
Bobby David <dartkietanmartaru@gmail.com>
2020-06-01 09:52:39 -0400

    Address comments.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   backendApi: window["env"]["backendApi"] || "http://174.138.57.106:81",
   submissionApi: window["env"]["submissionApi"] || "http://174.138.57.106:82",
 };
