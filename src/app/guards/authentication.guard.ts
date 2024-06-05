3c0f2dce7024edaa9df3b99f6e595fa80d5e2f5f
Bobby David <Bobby.David@hyland.com>
2024-06-05 09:51:32 -0400

    ng update @angular/core@16 @angular/cli@16

--- src/app/guards/authentication.guard.ts
+++ src/app/guards/authentication.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthenticationGuard implements CanActivate {
+export class AuthenticationGuard  {
 
   constructor(private auth: AuthService, private router: Router) { }
 
